6409f7f90b0d4c33f0129ed89942ad1517b2800b
Crozes Cyril <cyril.crozes@imerir.com>
2018-06-18 01:08:58 +0200

    API + LicencimentAuto.html

--- Server_API/licenciement-auto.py
+++ Server_API/licenciement-auto.py
@@ -1,6 +1,3 @@
-#174794343511-p7h4vpdfebh0h992hoqat80shsolh029.apps.googleusercontent.com
-#ABlzb89eGaZHLgiYLNZnQYRW
-
 from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
@@ -11,8 +8,6 @@ import os
 
 from flask import Flask
 app = Flask(__name__)
-
-
 
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
@@ -27,14 +22,13 @@ service = build('sheets', 'v4', http=creds.authorize(Http()))
 def Welcome():
     return "Hello World !"
     
-    
-@app.route('/sheetID/<string :post_id>')
+@app.route('/sheetID/<post_id>')
 def getSheetInfo(post_id):
     # Call the Sheets API
     #1vS3-iv0GOnHQTNMudK9yjl-KYdMQZjb7smJ6CNUa4x8
     SPREADSHEET_ID = post_id
     #NomDeLaFeuille!Range1:RangeTop
-    RANGE_NAME = 'Feuil1!A2:J'
+    RANGE_NAME = 'Réponses au formulaire 1!A2:Q'
     try :
         result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
                                                 range=RANGE_NAME).execute()
@@ -48,37 +42,42 @@ def getSheetInfo(post_id):
 
     if not values:
         print('No data found.')
+        return ''
     else:
-        #print('Nom, mail:')
-        try :
-            for row in values:
-                horodateur = row[0]
-                nom = row[1].upper()
-                prenom = row[2].upper()
-                sexe = row[3]
-                mail = row[4]
-                dateNaissance = row[5]
-                lieuNaissance = row[6]
-                adresse = row[7]
-                adresse2 = ''
-                if row[8] != NULL :
-                    adresse2 = row[8].upper()
-                codePostal = row[9]
-                ville = row[10]  
-                telephone = row[11]
-                commentaire = ''
-                if row[12] != NULL :
-                    commentaire = row[12]
-                activite = 'secourisme'    
-                result = {'Horodateur':horodateur,'Nom':nom,'Prenom':prenom,'Sexe':sexe,'Mail':mail,'DateNaissance':dateNaissance,'LieuNaissance':lieuNaissance,'Adresse':adresse,'Adresse2':adresse2,'CodePostal':codePostal,'Ville':ville,'Telephone':telephone,'Activite':activite}
-                resultFin.append(result)
-                #print('%s, %s' % (row[0], row[4]))
-        except:
-            print("Fin de valeurs")
-
+        for row in values:
+            print(row)
+            horodateur = row[0]
+            nom = row[1].upper()
+            prenom = row[2].upper()
+            nationalite = row[3]
+            dateNaissance = row[4]
+            lieuNaissance = row[5]
+            departement = row [6]
+            sexe = row[7]
+            adresse = row[8]
+            adresse2 = ''
+            if row[8] != None :
+                adresse2 = row[9]
+            codePostal = row[10]
+            ville = row[11]
+            pays = row[12]
+            telephone = ''
+            if row[13] != None :
+                telephone = '0'+row[13]
+            portable = ''
+            if row[14] != None :    
+                portable = '0'+row[14]
+            mail = row[15]
+            commentaire = ''
+            if row[16] != None :
+                commentaire = row[16]
+            activite = 'secourisme'    
+            result = {'Horodateur':horodateur,'Nom':nom,'Prenom':prenom,'Nationalite':nationalite,'Portable':portable,'Departement':departement,'Pays':pays,'Sexe':sexe,'Mail':mail,'DateNaissance':dateNaissance,'LieuNaissance':lieuNaissance,'Adresse':adresse,'Adresse2':adresse2,'CodePostal':codePostal,'Ville':ville,'Telephone':telephone,'Activite':activite, 'Commentaire':commentaire}
+            resultFin.append(result)
+            
         print(resultFin)
         return json.dumps(resultFin)
     
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))    
+	app.run(host='localhost', port=int(port))    
