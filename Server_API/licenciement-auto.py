ca2f030d2377ea39ad3f5069fc8da056b0c73367
Crozes Cyril <cyril.crozes@imerir.com>
2018-09-11 23:02:10 +0200

    Server API

--- Server_API/licenciement-auto.py
+++ Server_API/licenciement-auto.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*
+
 from __future__ import print_function
-from apiclient.discovery import build
+from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
@@ -7,6 +9,7 @@ import json
 import os
 
 from flask import Flask
+from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,12 +17,14 @@ app = Flask(__name__)
 #A tester
 cors = CORS(app)
 
+#--------------------_Function_--------------
+
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-store = file.Storage('credentials.json')
+store = file.Storage('/home/pi/Serveur/credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('/home/pi/Serveur/client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
@@ -28,13 +33,12 @@ def Welcome():
     return "Hello World !"
     
 @app.route('/sheetID/<post_id>')
-@crossdomain(origin='*')
 def getSheetInfo(post_id):
     # Call the Sheets API
     #1vS3-iv0GOnHQTNMudK9yjl-KYdMQZjb7smJ6CNUa4x8
     SPREADSHEET_ID = post_id
     #NomDeLaFeuille!Range1:RangeTop
-    RANGE_NAME = 'Réponses au formulaire 1!A2:Q'
+    RANGE_NAME = 'Réponses au formulaire 1!A2:P'
     try :
         result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
                                                 range=RANGE_NAME).execute()
@@ -50,6 +54,8 @@ def getSheetInfo(post_id):
         print('No data found.')
         return ''
     else:
+        print(values)
+        print("------")
         for row in values:
             print(row)
             horodateur = row[0]
@@ -58,32 +64,32 @@ def getSheetInfo(post_id):
             nationalite = row[3]
             dateNaissance = row[4]
             lieuNaissance = row[5]
-            departement = row [6]
+            departement = row[6]
             sexe = row[7]
             adresse = row[8]
             adresse2 = ''
-            if row[8] != None :
+            if row[9] != "" :
                 adresse2 = row[9]
             codePostal = row[10]
             ville = row[11]
             pays = row[12]
             telephone = ''
-            if row[13] != None :
+            if row[13] != "" :
                 telephone = '0'+row[13]
             portable = ''
-            if row[14] != None :    
+            if row[14] != "" :    
                 portable = '0'+row[14]
             mail = row[15]
             commentaire = ''
-            if row[16] != None :
-                commentaire = row[16]
+            #if row[16] != None :
+            #    commentaire = row[16]
             activite = 'secourisme'    
-            result = {'Horodateur':horodateur,'Nom':nom,'Prenom':prenom,'Nationalite':nationalite,'Portable':portable,'Departement':departement,'Pays':pays,'Sexe':sexe,'Mail':mail,'DateNaissance':dateNaissance,'LieuNaissance':lieuNaissance,'Adresse':adresse,'Adresse2':adresse2,'CodePostal':codePostal,'Ville':ville,'Telephone':telephone,'Activite':activite, 'Commentaire':commentaire}
+            result = {'Horodateur':horodateur,'Nom':nom,'Prenom':prenom,'Nationalite':nationalite,'Portable':portable,'Departement':departement,'Pays':pays,'Sexe':sexe,'Mail':mail,'DateNaissance':dateNaissance,'LieuNaissance':lieuNaissance,'Adresse':adresse,'Adresse2':adresse2,'CodePostal':codePostal,'Ville':ville,'Telephone':telephone,'Activite':activite}
             resultFin.append(result)
-            
+                
         print(resultFin)
-        return json.dumps(resultFin)
+        print("-----")
+        return jsonify(resultFin)
     
-port = os.getenv('PORT', '5000')
-if __name__ == "__main__":
-	app.run(host='localhost', port=int(port))    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
